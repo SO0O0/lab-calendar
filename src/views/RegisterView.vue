b62f01b451a0533a90ec0f81a09574180467433c
SO000000 <hirawatasou@gmail.com>
2022-08-05 17:35:00 +0900

    [update]register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="register">
+    <div class="form">
+    <!-- 名前フォーム -->
+            <div class="form-name">
+                <p>名前</p>
+                <input type="text" name="name" placeholder="名前" v-model="name">
+            </div>
+        <div>
+          <button disabled>送信する</button>
+        </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+#register{
+    text-align:center;
+}
+
+.form{
+  margin:0 auto;
+  width:40%;
+  background-color:whitesmoke;
+}
+
+.form-name{
+    padding:15px 0;
+    margin-bottom:30px;
+}
+
+input{
+    padding:5px 10px;
+    min-width:250px;
+}
+
+button{
+    width:20%;
+    background-color: #375a7f;
+    color:white;
+    padding:10px 0;
+    border:none;
+    border-radius:10px;
+    cursor:pointer;
+    margin-bottom: 50px;
+}
+</style>
+
+<script></script>
